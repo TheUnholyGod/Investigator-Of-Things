09b84292f6ea6951eae4754e5a447529938bf125
TheUnholyGod <kitsonlow99@gmail.com>
2018-08-28 10:56:39 +0300

    Project Commit

---
+++ Investigator of Things/Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable,CreateAssetMenu(fileName = "NewDialog")]
+public class Dialog : ScriptableObject {
+
+    [SerializeField]
+    string[] m_dialogs;
+
+}
