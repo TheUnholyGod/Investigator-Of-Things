265292dd7482b98d30aac5d0f112f8547cb8f3b4
TheUnholyGod <kitsonlow99@gmail.com>
2018-08-28 11:10:21 +0300

    Test Commit for Discord

---
+++ Investigator of Things/Assets/Scripts/Dialog/DialogManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogManager : MonoBehaviour {
+
+    [SerializeField]
+    Dialog m_currDialog;
+
+    public Dialog Dialog
+    {
+        get
+        {
+            return m_currDialog;
+        }
+
+        set
+        {
+            m_currDialog = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
