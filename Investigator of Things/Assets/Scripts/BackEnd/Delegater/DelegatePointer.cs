9bed943aee36bacf768b0e4bfca9bb5824af419c
TheUnholyGod <kitsonlow99@gmail.com>
2018-08-29 15:44:27 +0300

    Delegate pointers to add functionality to dialogue

---
+++ Investigator of Things/Assets/Scripts/BackEnd/Delegater/DelegatePointer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable, CreateAssetMenu(fileName = "Delegate", menuName = "Function/Delegate")]
+public class DelegatePointer : ScriptableObject {
+
+    [SerializeField]
+    UnityEvent m_function;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void Invoke()
+    {
+        m_function.Invoke();
+    }
+
+}
