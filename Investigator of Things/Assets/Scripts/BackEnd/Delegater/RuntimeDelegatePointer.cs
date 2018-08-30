ca4814a39caf956bdc1404ad5d1bc45148c5ae8f
TheUnholyGod <kitsonlow99@gmail.com>
2018-08-30 10:18:30 +0300

    Runtime Delegat Pointers

---
+++ Investigator of Things/Assets/Scripts/BackEnd/Delegater/RuntimeDelegatePointer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RuntimeDelegatePointer : MonoBehaviour {
+
+    [SerializeField]
+    UnityEvent m_function;
+
+    private void Awake()
+    {
+
+    }
+
+    public void Invoke()
+    {
+        m_function.Invoke();
+    }
+}
