87eb99f4a229f2ee6613ef97e37f378a7989d259
TheUnholyGod <kitsonlow99@gmail.com>
2018-08-30 13:28:57 +0300

    Basic Movemernt and Shooting for minigame done

---
+++ Investigator of Things/Assets/Scripts/Player/Bullet/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    float lifetime = 5;
+    float timer = 0;
+    Vector3 m_direction;
+    public Vector3 Direction
+    {
+        get
+        {
+            return m_direction;
+        }
+
+        set
+        {
+            m_direction = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+		if(timer < lifetime)
+        {
+            transform.position += Direction * 25 * Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
