1c7e13f0a851c2e90f587cb0265112913694ba5d
cjh2806 <cjh2806@gmail.com>
2018-09-06 11:06:16 +0300

    Droomba waypoints, calling Droomba after unlocking pc, stabbing droomba and cleaning up droomba

---
+++ Investigator of Things/Assets/Scripts/AI/DroombaAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroombaAI : AI {
+
+    public GameObject cleaningWaypoint;
+
+    public bool isCleaning = false;
+
+    public bool isImmobilized = false;
+
+    [SerializeField]
+    private GameObject startingPoint;
+
+    Vector3 defaultPos;
+    Quaternion defaultRotation;
+
+    InteractableObject interactable;
+
+    private void Start()
+    {
+        cleaningWaypoint.SetActive(false);
+        defaultPos = transform.position;
+        defaultRotation = transform.rotation;
+        interactable = this.GetComponent<InteractableObject>();
+        interactable.Dialogtree.MoveDown(4);
+
+        this.enabled = false;
+
+        this.transform.position = defaultPos;
+        this.transform.rotation = defaultRotation;
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        if (nextWaypoint.Equals(startingPoint))
+        {
+            this.enabled = false;
+            transform.position = defaultPos;
+            transform.rotation = defaultRotation;
+        }
+
+        base.OnTriggerEnter(other);
+
+        if (!nextWaypoint.activeSelf)
+            nextWaypoint = m_waypointSystem.GetNextWaypoint(nextWaypoint);
+
+        if (other.gameObject.Equals(cleaningWaypoint))
+        {
+            this.enabled = false;
+            nextWaypoint = null;
+            isCleaning = true;
+        }
+    }
+}
